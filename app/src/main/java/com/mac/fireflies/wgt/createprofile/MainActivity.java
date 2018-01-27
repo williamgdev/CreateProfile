1344cf1822913a65073852ef7395ff8619ec79f8
AndroidStorm <williamg.dev@gmail.com>
2018-01-26 20:10:41 -0600

    Add Login with Firebase

--- app/src/main/java/com/mac/fireflies/wgt/createprofile/MainActivity.java
+++ app/src/main/java/com/mac/fireflies/wgt/createprofile/MainActivity.java
@@ -3,13 +3,34 @@ package com.mac.fireflies.wgt.createprofile;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchLogin() {
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
 }
