b6f31ad40ab13816aaa9c25e782fe6906c98e1a3
AndroidStorm <williamg.dev@gmail.com>
2018-01-27 14:18:55 -0600

    Created Login/Logout

--- app/src/main/java/com/mac/fireflies/wgt/createprofile/MainActivity.java
+++ app/src/main/java/com/mac/fireflies/wgt/createprofile/MainActivity.java
@@ -2,17 +2,21 @@ package com.mac.fireflies.wgt.createprofile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 123;
     // Choose authentication providers
     List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -21,12 +25,17 @@ public class MainActivity extends AppCompatActivity {
     );
 
     TextView textView;
+    Button bLogout, bLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.greetings);
+        bLogIn = (Button) findViewById(R.id.button_logoin);
+        bLogout = (Button) findViewById(R.id.button_logout);
+        bLogIn.setOnClickListener(this);
+        bLogout.setOnClickListener(this);
         launchLogin();
     }
 
@@ -40,13 +49,43 @@ public class MainActivity extends AppCompatActivity {
                 RC_SIGN_IN);
     }
 
+    public void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        textView.setText("Thanks for us this app...");
+                    }
+                });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
             IdpResponse userData = IdpResponse.fromResultIntent(data);
-//            Toast.makeText(this, "User:" + userData.getEmail(), Toast.LENGTH_SHORT).show();
             textView.setText("Hello! " + userData.getEmail());
+            bLogIn.setVisibility(View.GONE);
+            bLogout.setVisibility(View.VISIBLE);
+        } else {
+            bLogout.setVisibility(View.GONE);
+            bLogIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_logout:
+                logout();
+                bLogout.setVisibility(View.GONE);
+                bLogIn.setVisibility(View.VISIBLE);
+                break;
+            case R.id.button_logoin:
+                launchLogin();
+                bLogout.setVisibility(View.VISIBLE);
+                bLogIn.setVisibility(View.GONE);
+                break;
         }
     }
 }
